984d329c4d3498e3b85e361fab49a695dfd6f5e4
fazlerabbi125 <fazle.rabbi125faiyaz@gmail.com>
2022-12-29 23:37:20 +0600

    Recipe Detail day-2

--- components/molecules/RecipeAutoComplete.tsx
+++ components/molecules/RecipeAutoComplete.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Autocomplete, AutocompleteItem } from "@mantine/core";
 import { useDebouncedValue } from "@mantine/hooks";
 import { fetchData } from "../../hooks/useAxios";
-import { useRouter } from "next/router";
+import { withRouter, NextRouter } from "next/router";
 import { AxiosError } from "axios";
 import { RecipeAutoCompleteType } from "../../pages/api/recipes/autocomplete";
 
@@ -10,40 +10,52 @@ interface SearchResult extends AutocompleteItem {
     slug: string;
 }
 
-const RecipeAutoComplete: React.FC = () => {
-    const router = useRouter();
+const RecipeAutoComplete: React.FC<{ router: NextRouter }> = ({ router }) => {
     const [search, setSearch] = React.useState("");
-    const [debouncedSearch] = useDebouncedValue(search, 500);
     const [searchResults, setSearchResults] = React.useState<SearchResult[]>([]);
+
+    const [debouncedSearch] = useDebouncedValue(search, 1000);
 
     const redirectToRecipe = (item: SearchResult) => {
         router.push(`/recipes/${item.slug}`);
-        setSearch("")
+        setSearch("");
     };
 
     React.useEffect(() => {
+        let isApiSubscribed = true;
         if (debouncedSearch.length > 0) {
             fetchData("/recipes/autocomplete", {
                 search: debouncedSearch,
             })
                 .then((data: RecipeAutoCompleteType) => {
-                    const results = data.map((elem) => ({
-                        slug: elem.slug,
-                        value: elem.name,
-                    }));
-                    // console.log(data);
-                    setSearchResults(results);
+                    if (isApiSubscribed) {
+                        const results = data.map((elem) => ({
+                            slug: elem.slug,
+                            value: elem.name,
+                        }));
+                        setSearchResults(results);
+                        console.log(results, searchResults);
+                    }
                 })
                 .catch((err: AxiosError) => {
                     console.log(err.message);
                     setSearchResults([]);
                 });
-        } else setSearchResults([]);
+        } else {
+            setSearchResults([]);
+        }
+
+        return () => {
+            // cancel the subscription
+            isApiSubscribed = false;
+        };
     }, [debouncedSearch]);
+
+    console.log(searchResults);
 
     return (
         <Autocomplete
-            placeholder="Search recipes"
+            placeholder="Search recipes by name or ingredients"
             radius="xs"
             data={searchResults}
             nothingFound={debouncedSearch ? "No recipes found" : ""}
@@ -51,7 +63,7 @@ const RecipeAutoComplete: React.FC = () => {
             limit={searchResults.length}
             onChange={setSearch}
             onItemSubmit={redirectToRecipe}
-            className="sm:mr-2 min-w-[105%]"
+            className="sm:mr-2 min-w-[17rem]"
             classNames={{
                 dropdown: "max-h-60 overflow-y-auto",
             }}
@@ -59,4 +71,4 @@ const RecipeAutoComplete: React.FC = () => {
     );
 };
 
-export default RecipeAutoComplete;
+export default withRouter(RecipeAutoComplete);
