f68050e6851e0c242f0c7bde4d23ae051b0a5102
fazlerabbi125 <fazle.rabbi125faiyaz@gmail.com>
2022-12-31 22:13:24 +0600

    Recipe Details day-3

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import { Card } from "@mantine/core";
+import Head from "next/head";
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+      <div>About</div>
+    </>
+  );
+};
+
+export default About;
