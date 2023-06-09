8ce8845dce902f9efaadbf4575894d7a1e3c100b
fazlerabbi125 <fazle.rabbi125faiyaz@gmail.com>
2023-06-09 22:11:22 +0600

    Add og:description to DefaultMeta

--- seo-tags/metadata/DefaultMeta.tsx
+++ seo-tags/metadata/DefaultMeta.tsx
@@ -1,16 +1,37 @@
 import Head from "next/head";
 
 export default function DefaultMeta() {
-    const siteName = "Faiyaz's Recipes";
+    const data = {
+        siteName: "Faiyaz's Recipes",
+        description: "A demo recipe app created using Next.js",
+        url: process.env.NEXT_PUBLIC_HOST || "",
+        image: (process.env.NEXT_PUBLIC_HOST || "") + "/assets/images/enjoy-food.jpg"
+    }
     return (
         <Head>
-            <title>{siteName}</title>
-            <meta property="og:title" content={siteName} />
+            <meta property="og:title" content={data.siteName} />
             {/*Default og:type is website i.e. <meta property="og:type" content="website" /> */}
-            <meta property="og:url" content={process.env.NEXT_PUBLIC_HOST} />
-            {/* If next/image is not used, imported image source is found from IMG_IMPORT.src */}
-            <meta property="og:image" content={process.env.NEXT_PUBLIC_HOST + "/assets/images/cutlery.svg"} />
-            <meta property="og:site_name" content={siteName} />
+            <meta property="og:url" content={data.url} />
+            <meta property="og:image" content={data.image} />
+            <meta property="og:site_name" content={data.siteName} />
+            <meta
+                property="og:description"
+                content={data.description}
+            />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={data.siteName} />
+            <meta name="twitter:description" content={data.description} />
+            <meta name="twitter:image" content={data.image} />
+            <meta name="twitter:site" content={data.siteName} />
+
+            <title>{data.siteName}</title>
+            <meta name="title" content={data.siteName} />
+            <meta
+                name="description"
+                content={data.description}
+            />
+            <meta name="url" content={data.url} />
         </Head>
     );
 }
