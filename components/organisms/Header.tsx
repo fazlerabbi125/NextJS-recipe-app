db9e1f166fe835f3f7db12b9d0dc7c4c02281ec1
fazlerabbi125 <fazle.rabbi125faiyaz@gmail.com>
2023-07-30 15:18:35 +0600

    Header props update

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Title } from "@mantine/core";
+
 interface HeaderProps {
-    className?: string;
+    classNames?: {
+        container?: string;
+        text?: string;
+    };
     children: React.ReactNode;
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
     return (
-        <header className="text-zinc-50 font-bold">
-            <Title className={props.className}>{props.children}</Title>
+        <header
+            className={["text-zinc-50", "font-bold", props.classNames?.container]
+                .filter((elem) => elem)
+                .join(" ")}
+        >
+            <Title className={props.classNames?.text}>{props.children}</Title>
         </header>
     );
 };
